8aa4bcf7d4363b2ea63db0d806754e515403ef1f
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2022-06-11 14:39:32 +0530

    d

--- player.sh
+++ player.sh
@@ -1,6 +1,14 @@
 #!/bin/fish
-       if test (playerctl status) = "Playing"
-           echo $(playerctl metadata --format "Now Playing: {{ title }} - {{ artist }}")
-       else 
-           echo ""
-		end
+if test (playerctl status) = "Playing"
+    set p $(playerctl metadata --format "Now Playing: {{ title }} - {{ artist }}")
+    if test (string length $p) -gt 60
+        set p $(playerctl metadata --format "Now Playing: {{ title }}")
+        if test (string length $p) -gt 60
+            echo "$(string sub --length 60 $p)..."
+        else
+            echo $p
+        end
+    else
+        echo $p
+    end
+end
