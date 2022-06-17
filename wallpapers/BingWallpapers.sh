49593c7af7b01e1300d19875e47921d9f4adbdb0
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2022-06-18 01:02:53 +0530

    Auto Commit

--- wallpapers/BingWallpapers.sh
+++ wallpapers/BingWallpapers.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
+export HOME=/home/zed
 urlpath=$( \
 curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
 | xmllint --xpath "/rss/channel/item/link/text()" - \
 | sed 's/1366x768/1920x1080/g' \
 )
 curl "https://www.bing.com$urlpath" \
-| feh --bg-fill -
+| feh --bg-fill - || feh --bg-fill - /Zed/Linux/sweet/assets/bg.jpg
