16a87f92e034db419a41987434c8791db327d144
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2022-07-15 10:34:05 +0530

    Auto Commit

--- config/xmobar/sound.sh
+++ config/xmobar/sound.sh
@@ -7,7 +7,7 @@ function get_bar
     if [ $br -gt 99 ]
         set -gx sound "$br"
     else if [ $br -gt 9 ]
-        set -gx sound "$br"
+        set -gx sound "$br "
     else
         set -gx sound "$br"
     end
