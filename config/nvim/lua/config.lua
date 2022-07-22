22f3d136dde02bc1b5b028b4b35d26f0f02b5401
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2022-07-22 21:33:01 +0530

    Auto Commit

--- config/nvim/lua/config.lua
+++ config/nvim/lua/config.lua
@@ -4,6 +4,26 @@ require('onedark').setup {
 }
 require('onedark').load()
 
+
+local autosave = require("autosave")
+
+autosave.setup(
+    {
+        enabled = true,
+        execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+        events = {"InsertLeave", "TextChanged"},
+        conditions = {
+            exists = true,
+            filename_is_not = {},
+            filetype_is_not = {},
+            modifiable = true
+        },
+        write_all_buffers = false,
+        on_off_commands = true,
+        clean_command_line_interval = 0,
+        debounce_delay = 135
+    }
+)
 -----------------------------------------------------------
 -- Define keymaps of Neovim and installed plugins.
 -----------------------------------------------------------
