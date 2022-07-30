0b045aedde41d14afd7eaf74e00afa24ffdd5742
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2022-07-30 19:52:00 +0530

    Auto Commit

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -179,3 +179,4 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
  alias cls="clear"
  set -x xmonadrc "/home/zed/.config/xmonad/xmonad.hs"
  set -x xmobarrc "/home/zed/.config/xmobar/xmobarrc"
+ thefuck --alias f | source
