9643457963eaf8785dc84576904173f600428567
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2022-08-17 21:15:14 +0530

    Auto Commit

--- config/fish/functions/op.fish
+++ config/fish/functions/op.fish
@@ -3,9 +3,11 @@ argparse "m" -- $argv
 or return 
 if set -q _flag_m 
 for i in $argv
-        nohup $i &> /dev/null & disown 
+        $i &> /dev/null & disown 
 end
 else
-(string split " " $argv) &> /dev/null & disown
+for i in  (string split " " $argv)
+ $i &> /dev/null & disown
+end
 end
 end
