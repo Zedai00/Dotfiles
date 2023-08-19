12ad529e2546ff82a87c20028d8ee80cbf6707af
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2023-08-20 00:48:44 +0530

    WSL

--- config/sway/BingWallpapers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/fish
-set wall ~/.dotfiles/wallpapers/wall.jpg
-set urlpath ( \
-curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
-| xmllint --xpath "/rss/channel/item/link/text()" - \
-| sed 's/1366x768/1920x1080/g' \
-)
-curl "https://www.bing.com$urlpath" -o $wall
-swaymsg output "*" bg ~/.dotfiles/wallpapers/wall.jpg fill
--- config/sway/lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/fish
-set lock 'swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %e.%m.%Y" --timestr "%k:%M"'
-
-exec swayidle -w \
-    timeout 300 $lock \
-    timeout 600 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-    before-sleep $lock
