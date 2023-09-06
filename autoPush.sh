d75fcc4bcd917623fb41c20a3d7c8d343ebf7d17
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2023-09-06 23:59:29 +0530

    Auto Commit

--- autoPush.sh
+++ autoPush.sh
@@ -1,7 +1,7 @@
 #!/bin/fish
 while inotifywait -e modify,create,delete -r /home/zed/.dotfiles/
     cd /home/zed/.dotfiles/
-    git add -A &> ~/log 
-    git commit -m "Auto Commit" &> ~/log
+    git add -A >> ~/log 
+    git commit -m "Auto Commit" >> ~/log
     git push >> ~/log  
 end
